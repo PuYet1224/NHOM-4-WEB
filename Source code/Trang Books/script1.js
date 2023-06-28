39d8c7f2a04d8653900465645689bcc0db6a7d17
HoangPzzz <rongcutodz123@gmail.com>
2023-06-28 07:20:42 +0700

    add

---
+++ Source code/Trang Books/script1.js
@@ -0,0 +1,21 @@
+function img1(){
+    document.getElementById("mainimg").src="img/imgs/1.webp";
+}
+function img2(){
+    document.getElementById("mainimg").src="img/imgs/1.2";
+}
+function img3(){
+    document.getElementById("mainimg").src="img/imgs/1.3";
+}
+function img4(){
+    document.getElementById("mainimg").src="img/imgs/1.4";
+}
+function img5(){
+    document.getElementById("mainimg").src="img/imgs/1.5";
+}
+function img6(){
+    document.getElementById("mainimg").src="img/imgs/1.6.avif";
+}
+function img7(){
+    document.getElementById("mainimg").src="img/imgs/1.7.avif";
+}
